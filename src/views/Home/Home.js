0679e80edfc2ba2081da5b4b7dfe5fe0d3e3a846
Kamie Short <kamie.short@gmail.com>
2022-03-09 13:03:38 -0800

    working on Onchange event listener

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,29 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Preview from '../../components/Preview/Preview';
 import Editor from '../../components/Editor/Editor';
 
 import './Home.css';
-import { useState } from 'react/cjs/react.production.min';
 
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
-  const [title, setTitle] = useState('Title');
-  const [subtitle, setSubtitle] = useState('SubTitle');
-  const [font, setFont] = useState('');
-  const [align, setAlign] = useState('');
+  const [title, setTitle] = useState('');
+  const [subtitle, setSubtitle] = useState('');
+  const [font, setFont] = useState('Titillium');
+  const [align, setAlign] = useState('center');
   const [text, setText] = useState('');
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview
+      <Preview title={title} subtitle={subtitle} font={font} align={align} text={text} />
+      <Editor
         setTitle={setTitle}
         setSubtitle={setSubtitle}
         setFont={setFont}
         setAlign={setAlign}
         setText={setText}
       />
-      <Editor title={title} subtitle={subtitle} font={font} align={align} text={text} />
     </main>
   );
 }
