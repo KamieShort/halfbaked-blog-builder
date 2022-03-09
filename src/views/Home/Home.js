045b80c9b4760ce4e553cf0cecb5096916464e63
Kamie Short <kamie.short@gmail.com>
2022-03-09 12:37:02 -0800

    woring on passing state variables as props

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -4,15 +4,26 @@ import Preview from '../../components/Preview/Preview';
 import Editor from '../../components/Editor/Editor';
 
 import './Home.css';
+import { useState } from 'react/cjs/react.production.min';
 
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
-
+  const [title, setTitle] = useState('Title');
+  const [subtitle, setSubtitle] = useState('SubTitle');
+  const [font, setFont] = useState('');
+  const [align, setAlign] = useState('');
+  const [text, setText] = useState('');
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview />
-      <Editor />
+      <Preview
+        setTitle={setTitle}
+        setSubtitle={setSubtitle}
+        setFont={setFont}
+        setAlign={setAlign}
+        setText={setText}
+      />
+      <Editor title={title} subtitle={subtitle} font={font} align={align} text={text} />
     </main>
   );
 }
